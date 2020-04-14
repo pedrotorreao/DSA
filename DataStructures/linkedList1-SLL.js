abd29b9788f9c8206af1551618cb13da515c2359
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2020-04-14 05:40:39 -0300

    adding insert() method

--- DataStructures/linkedList1-SLL.js
+++ DataStructures/linkedList1-SLL.js
@@ -35,13 +35,42 @@ class LinkedList {
     this.length++;
   }
   prepend(value) {
-    const first = {
+    const first = { //It could be a good to create a generic method/class for the creation of new nodes
       value: value,
       next: null
     };
     first.next = this.head;
     this.head = first;
     this.length++;
+  }
+  insert(value, index = 1) { //If no argument is passed as index, assume zero value
+    if (index <= 1) {
+      this.prepend(value);
+    } else if (index >= this.length) {
+      this.append(value);
+    } else {
+      const node = {
+        value: value,
+        next: null
+      };
+      let currentNode = this.head;
+      for (let i = 1; i < index-1; i++) {
+        currentNode = currentNode.next; //Setting the temporary node to be equal to the 'before node' where we must insert the new element
+      }
+      node.next = currentNode.next; //Linking the new node to the next node
+      currentNode.next = node; //Finish the insertion by linking the 'previous node' to our new node
+      this.length++;
+    }
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    console.log(array);
   }
 }
 
@@ -51,7 +80,13 @@ myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
 myLinkedList.prepend(8);
-console.log(myLinkedList);
+myLinkedList.printList();
+myLinkedList.insert(0,3);
+myLinkedList.insert(0,5);
+myLinkedList.insert(0,1);
+myLinkedList.insert(23);
+myLinkedList.printList();
+//console.log(myLinkedList);
 
 //JavaScript -- Variable Assignment
 // 1) Primitive values (numbers, strings, undefined, boolean, null): assign by value,
