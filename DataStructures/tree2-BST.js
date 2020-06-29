a67c7c6549447a61165ef8458c20d4444d32d366
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2020-06-29 20:55:53 -0300

    bst implementation using recursion

---
+++ DataStructures/tree2-BST.js
@@ -0,0 +1,87 @@
+/****************************************************************/
+/* Trees - Binary Search Tree (BST): Using recursion  ********/
+/****************************************************************/
+
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    let node = new Node(value);
+    const currentNode = this.root;
+
+    if (this.root === null) {
+      this.root = new Node(value);
+      return;
+    } 
+    else {
+      const searchTree = function(currentNode) {
+        if(value < currentNode.value) {
+          if(currentNode.left === null) {
+            currentNode.left = node;
+            return;
+          } 
+          return searchTree(currentNode.left);
+        } 
+        else if(value > currentNode.value) {
+          if(currentNode.right === null) {
+            currentNode.right = node;
+            return;
+          }
+          return searchTree(currentNode.right);
+        } 
+        else {
+          return null;
+        }
+      };
+      return searchTree(currentNode);
+    }
+  }
+  // lookup(value) {
+
+  // }
+  // remove(value) {
+
+  // }
+  // isBalanced() {
+
+  // }
+  // minHeight() {
+
+  // }
+  // maxHeight() {
+
+  // }
+  // inOrderTraversal() {
+
+  // }
+  // preOrderTraversal() {
+
+  // }
+  // postOrderTraversal() {
+
+  // }
+  // levelOrderTraversal() {
+
+  // }
+}
+//     9
+//  4     20
+//1  6  15  170
+const tree = new BinarySearchTree();
+//Create tree above:
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+console.log(tree);
