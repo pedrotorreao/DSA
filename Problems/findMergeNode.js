6b1214363a2a0734e58e22b2e989aca0bbcb6398
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2020-07-29 22:17:28 -0300

    add explanation

--- Problems/findMergeNode.js
+++ Problems/findMergeNode.js
@@ -29,3 +29,24 @@ function findMergeNode(headA, headB) {
   }
   return currentNodeB.data;
 }
+/*
+-- Explanation:
+Say listA is x long and merges at j. And the other listB is y long and merges at k.
+One iterator will take j steps to hit the merge point in listA, then (x-j) steps to hit the end of listA, then it will go to the listB and take k steps to hit its merge point. Total1 = j + (x-j) + k.
+The other iterator will take k steps to hit the merge point of listB and (y-k) to hit the end of listB, then will go to the listA and take j steps to hit its merge point. Total2 = k + (y-k) + j.
+Since the second part of both lists is the same sequence, (x-j) is the same as (y-k). Call that number q. Plugging that into both totals gives (j + q + k) for the first and (k + q + j) for the second. Which are obviously equal. So both iterators travel the same amount of steps in other to get to the merge points in both lists. Example:
+llA: 1-->2-->7-->3-->11-->NULL
+llB: 6-->10-->11-->NULL
+merge:
+1--2--7--3
+          \
+          11--NULL
+          /
+    6--10  
+Iterator 1: 1--2--7--3--11--(NULL)--6--10--11
+Iterator 2: 6--10--11--(NULL)--1--2--7--3--11
+
+Both iterators got to the merge point (11) in both lists in the same number of steps (9, counting NULL):
+Total1 = j + (x-j) + k = 5 + (6-5) + 3 = 9
+Total2 = k + (y-k) + j = 3 + (4-3) + 5 = 9
+*/
