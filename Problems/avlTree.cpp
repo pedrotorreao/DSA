eb1174405954ab9e1577c2bf4930b0881bb223e7
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2020-10-12 02:23:13 -0300

    solution update: last 3 test cases getting segmentation faults

--- Problems/avlTree.cpp
+++ Problems/avlTree.cpp
@@ -1,6 +1,3 @@
-#include <iostream>
-using namespace std;
-
 /* Node is defined as :
 typedef struct node
 {
@@ -21,18 +18,41 @@ node * newNode(int val)
     
     return newNode;
 }
-node * getHeight(node * root)
+int getHeight(node* root)
 {
-    if(root == NULL) {
+    if(root == nullptr) {
         return -1;
     } else {
         return root->ht;
     }
 }
+node* rightRotate(node* root)
+{
+    node* temp = root->left;
+    root->left = temp->right;
+    temp->right = root;
+
+    root->ht = 1 + max(getHeight(root->left), getHeight(root->right));
+    temp->ht = 1 + max(getHeight(temp->left), getHeight(temp->right));
+
+    return temp;
+}
+node* leftRotate(node* root)
+{
+    node* temp = root->right;
+    root->right = temp->left;
+    temp->left = root;
+
+    root->ht = 1 + max(getHeight(root->left), getHeight(root->right));
+    temp->ht = 1 + max(getHeight(temp->left), getHeight(temp->right));
+
+    return temp;
+}
 node * insert(node * root,int val)
 {
-	if(root == NULL) {
-        root = newNode(val); return root;
+    if(root == nullptr) {
+        root = newNode(val); 
+        return root;
     }
     if(val > root->val) {
         root->right = insert(root->right, val);
@@ -40,11 +60,36 @@ node * insert(node * root,int val)
     else if(val < root->val) {
         root->left = insert(root->left, val);
     }
-    else{ return nullptr; }
+    //else{ return 0; }
     
     root->ht = 1 + max(getHeight(root->left),getHeight(root->right));
   
     int balance = getHeight(root->left) - getHeight(root->right);
     
-    
+    if(root->left != nullptr && balance > 1) {//Left subtree disbalanced
+        if(val < root->left->val) { 
+            //Left-Left case: perform a right rotation on the disb. node
+            return rightRotate(root);
+        }
+        else {
+            //Left-Right case: perfom a left rotation on the disb. node left subtree
+            //and a right rotation on the disb. node
+            root->left = leftRotate(root->left);
+            return rightRotate(root);
+
+        }
+    }
+    if(root->right != nullptr && balance < -1) {//Right subtree disbalanced
+        if(val > root->right->val) { 
+            //Right-Right case: perform a left rotation on the disb. node
+            return leftRotate(root);
+        }
+        else {
+            //Right-Left case: perfom a right rotation on the disb. node right subtree
+            //and a left rotation on the disb. node
+            root->right = rightRotate(root->left);
+            return leftRotate(root);
+        }
+    }
+    return root;
 }
