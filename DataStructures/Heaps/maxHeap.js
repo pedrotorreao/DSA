6b788b599cd26b8061248c578284fba5c3012333
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2020-11-09 21:36:35 -0300

    add check for right child undefined

--- DataStructures/Heaps/maxHeap.js
+++ DataStructures/Heaps/maxHeap.js
@@ -14,32 +14,38 @@ class maxHeap {
   // -----------------------------------------------------------<<
   // ------------ MAX HEAP: INSERTION METHODS ------------------>>
   insert(value) {
-    if(this.heap.length < 1) {
+    if(this.heap.length < 1) { 
+      //if heap is empty, set first array element to null and push value to the end:
       this.heap[0] = null;
       this.heap.push(value);
       return;
     }
     this.heap.push(value);
-    this.fixInsertRecursively();
-    // this.fixInsertIteratively();
+    this.heapifyBottomTopRecursively();
+    //this.heapifyBottomTopIteratively();
   }
-  fixInsertRecursively(index=this.heap.length-1) {
+  heapifyBottomTopRecursively(index=this.heap.length-1) {
       let parentIndex = Math.floor(index/2);
+      //in case we've reached the root of the heap, return:
       if(!this.heap[parentIndex]) return;
 
+      //if parent value is less than the newly added element, swap them and go up in the heap:
       if(this.heap[parentIndex] < this.heap[index]) {
         this.swapValues(index, parentIndex);
         index = parentIndex;
-        this.fixInsertRecursively(index);
+        this.heapifyBottomTopRecursively(index);
       }
       else { return; }
   }
-  fixInsertIteratively() {
+  heapifyBottomTopIteratively() {
     let index = this.heap.length-1;
     let parentIndex;
     while(index >= 1){
       parentIndex = Math.floor(index/2);
+      //in case we've reached the root of the heap, return:
       if(!this.heap[parentIndex]) return;
+
+      //if parent value is less than the newly added element, swap them and go up in the heap:
       if(this.heap[parentIndex] < this.heap[index]) {
         this.swapValues(index, parentIndex);
         index = parentIndex;
@@ -52,24 +58,32 @@ class maxHeap {
   // ------------ MAX HEAP: DELETION METHODS ------------------->>
   
   remove() {
-    if(this.heap.length <= 1) { return; }
+    if(this.heap.length <= 1) { return; } //heap is empty.
 
-    this.swapValues(this.heap.length-1,1); //Replaces top element by the last element in the heap
+    //Replaces top element by the last element in the heap:
+    this.swapValues(this.heap.length-1,1); 
     this.heap.pop();
 
-    // this.fixRemoveIteratively();
-    this.fixRemoveRecursively();
+    this.heapifyTopBottomIteratively();
+    //this.heapifyTopBottomRecursively();
   }
   
-  fixRemoveIteratively() {
+  heapifyTopBottomIteratively() {
     let rootIndex, leftIndex, rightIndex;
 
     rootIndex = 1;
     leftIndex = 2*rootIndex;
     rightIndex = (2*rootIndex)+1;
 
+    //if there are no children, return. Reminder: as the heap must be a complete binary tree, all its levels are 
+    //filled except possibly for the left, and in the last level, all the keys/values are aligned as left as 
+    //possible, so if a node doesn't have a left child, it won't have a right child since the left subtree would 
+    //be filled first:
+    if(!this.heap[leftIndex]) { return; }
+
     while(this.heap[rootIndex] < this.heap[leftIndex] || this.heap[rootIndex] < this.heap[rightIndex]) {
-      if(this.heap[leftIndex] > this.heap[rightIndex]) {
+      //if there's no right child or left child's value greater than right child's, swap subtrees' root and left ////child's values:
+      if(!this.heap[rightIndex] || this.heap[leftIndex] > this.heap[rightIndex]) {
         this.swapValues(rootIndex, leftIndex);
         rootIndex = leftIndex;
       }
@@ -84,16 +98,22 @@ class maxHeap {
     }
   }
 
-  fixRemoveRecursively(rootIndex=1) {
+  heapifyTopBottomRecursively(rootIndex=1) {
     if(!this.heap[rootIndex]) { return; }
 
     let leftIndex, rightIndex;
-
     leftIndex = 2*rootIndex;
     rightIndex = (2*rootIndex)+1;
 
+    //if there are no children, return. Reminder: as the heap must be a complete binary tree, all its levels are 
+    //filled except possibly for the left, and in the last level, all the keys/values are aligned as left as 
+    //possible, so if a node doesn't have a left child, it won't have a right child since the left subtree would 
+    //be filled first:
+    if(!this.heap[leftIndex]) { return; }
+
     if(this.heap[rootIndex] < this.heap[leftIndex] || this.heap[rootIndex] < this.heap[rightIndex]) {
-      if(this.heap[leftIndex] > this.heap[rightIndex]) {
+      //if there's no right child or left child's value greater than right child's, swap subtrees' root and left ////child's values:
+      if(!this.heap[rightIndex] || this.heap[leftIndex] > this.heap[rightIndex]) {
         this.swapValues(rootIndex, leftIndex);
         rootIndex = leftIndex;
       }
@@ -101,7 +121,7 @@ class maxHeap {
         this.swapValues(rootIndex, rightIndex);
         rootIndex = rightIndex;
       }
-      this.fixRemoveRecursively(rootIndex);
+      this.heapifyTopBottomRecursively(rootIndex);
     }
   }
   
@@ -130,6 +150,8 @@ Heap.insert(1);
 console.log('Max Heap size: ', Heap.getSize());
 console.log('Max/Top element: ', Heap.peek());
 
+Heap.remove();
+Heap.remove();
 Heap.remove();
 
 console.log('Max Heap size: ', Heap.getSize());
