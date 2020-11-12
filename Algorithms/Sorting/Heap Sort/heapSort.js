266c738b538c3c8f6cc7f1248dea5637580e8bba
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2020-11-11 23:43:18 -0300

    add comment

--- Algorithms/Sorting/Heap Sort/heapSort.js
+++ Algorithms/Sorting/Heap Sort/heapSort.js
@@ -11,6 +11,9 @@ class HeapSort {
       Heap.insert(element);
     });
 
+    /* 
+    When removing an element in a heap, we always remove the top element. As in this case we have a min heap, the top element is always the smallest element in the heap. If every time we remove an element from the heap, we return the deleted value and add it to an array, in the end we'll have created a sorted array in incresing order.
+    */
     while(Heap.getSize()) {
       sortedArray.push(Heap.remove());
     }
