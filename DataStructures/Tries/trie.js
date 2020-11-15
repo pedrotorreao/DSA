6835c4784d67537c02032082be40473646962c78
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2020-11-15 02:02:00 -0300

    add  insert method

--- DataStructures/Tries/trie.js
+++ DataStructures/Tries/trie.js
@@ -12,10 +12,14 @@ class Trie {
     this.root = new Node();
   }
   searchWord(str){
-
-  }
-  insertWord(str){
     
+  }
+  insertWord(str, node = this.root){
+    if(!str.length) { node.endOfWord = true; return; }
+    if(!node.keys.has(str[0])){
+      node.keys.set(str[0], new Node());
+    }
+    this.insertWord(str.slice(1), node.keys.get(str[0]));
   }
   removeWord(str){
 
@@ -31,4 +35,23 @@ class Trie {
   }
 }
 const myTrie = new Trie();
+myTrie.insertWord('man');
+myTrie.insertWord('mind');
+myTrie.insertWord('mid');
+myTrie.insertWord('apple');
+myTrie.insertWord('tree');
+myTrie.insertWord('town');
 console.log(myTrie);
+/*
+          root
+      -------------
+      /     |     \
+    m       a      t
+  /  \      |    /  \
+a     i     p   r    o
+|   /  \   |    |     | 
+n* n   d*  p    e     w
+  |        |    |     |
+  d*       e*   e*   n*
+
+*/
