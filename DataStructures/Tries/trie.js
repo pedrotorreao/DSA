0f00757e743dbb5ebd5cef3d547255a34419615e
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2020-11-18 21:51:08 -0300

    add printTrie() method

--- DataStructures/Tries/trie.js
+++ DataStructures/Tries/trie.js
@@ -11,16 +11,27 @@ class Trie {
   constructor() {
     this.root = new Node();
   }
-  searchPrefix(str, node = this.root){
+  searchPrefix(prefix, node = this.root){
     /*  
       - Descr.: Print out all the words containing a specific prefix.
       - Input:  It receives the prefix it must look for as input.
       - Output: It stores all the words which begin with that prefix and print them out.
     */
-
-    if(!str.length) { console.log('The prefix string is empty.'); return; }
-    let prefix = [];
-
+    if(!prefix.length || !node.keys.has(prefix[0] || !node.keys.size)) { console.log('Not present.'); return; }
+    let prefixMatch = new Array(); //stores all the complete words matching this prefix.
+    let str = new String();
+    const search = function(node,prefix,str){
+      if(node.keys.has(prefix[0])){
+          let charac = prefix[0];
+          str.concat(charac)
+          prefix.slice(1); console.log(str);
+          search(node.keys.get(prefix[0]), prefix, str);
+        if(node.endOfWord){
+          prefixMatch.push(str);
+        }
+      }
+    }; search(this.root,prefix,str);
+    console.log(str);
   }
   insertWord(str, node = this.root){
     /*
@@ -55,12 +66,23 @@ class Trie {
       - Output: Returns a boolean indicating whether the word is complete or not.
     */
   }
-  print(){
+  printTrie(){
     /*
       - Descr.: Prints out all the words on the trie (Try to print in alphabetical order later).
       - Input:  - 
       - Output: All the words on the trie are logged on the screen.
     */
+    let words = new Array(); //stores all the complete words matching this prefix.
+    let str = new String();
+
+    const search = function(node, string){
+      for(let letter of node.keys.keys()){
+        search(node.keys.get(letter), string.concat(letter));
+      }
+      if(node.endOfWord){ words.push(string); }
+    };
+    search(this.root, str);
+    return words;
   }
 }
 const myTrie = new Trie();
@@ -70,7 +92,10 @@ myTrie.insertWord('mid');
 myTrie.insertWord('apple');
 myTrie.insertWord('tree');
 myTrie.insertWord('town');
+myTrie.insertWord('middle');
 console.log(myTrie);
+console.log(myTrie.printTrie());
+// myTrie.searchPrefix('mi');
 /*
           root
       -------------
@@ -80,7 +105,11 @@ console.log(myTrie);
 a     i     p   r    o
 |   /  \   |    |     | 
 n* n   d*  p    e     w
-  |        |    |     |
-  d*       e*   e*   n*
+  |    |   |    |     |
+  d*   d   e*   e*   n*
+      |
+      l
+      |
+      e*
 
 */
