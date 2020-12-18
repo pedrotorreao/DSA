44aaf47445221883c41b58a9f5b0d51261c2e98b
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2020-12-17 23:24:38 -0300

    additional tests

--- Algorithms/Sorting/Bubble Sort/bubbleSort1.js
+++ Algorithms/Sorting/Bubble Sort/bubbleSort1.js
@@ -12,5 +12,31 @@ function bubbleSort(array){
   }
   return array;
 }
-let testArray = [9,85,-5,10,0,-17,123,3,67];
-console.log(bubbleSort(testArray));
+
+//TEST CASES:
+//1-:
+// let testArray1 = [9,85,-5,10,0,-17,123,3,67];
+
+// console.time('Execution Time');
+// let orderedArray1 = bubbleSort(testArray1);
+// console.timeEnd('Execution Time');
+
+// console.log(orderedArray1);
+
+//2-:
+// let testArray2 = [593,12,0,-45,1,45,23,-435,-1];
+
+// console.time('Execution Time');
+// let orderedArray2 = bubbleSort(testArray2);
+// console.timeEnd('Execution Time');
+
+// console.log(orderedArray2);
+
+//3-:
+let testArray3 = [722,345,299,122,119,90,88,51,23,1,0,-1,-24,-121,-987];
+
+console.time('Execution Time');
+let orderedArray3 = bubbleSort(testArray3);
+console.timeEnd('Execution Time');
+
+console.log(orderedArray3);
