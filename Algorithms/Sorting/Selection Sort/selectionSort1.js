f54207b84262aeb6144a1736292048ba19c40512
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2020-12-18 23:59:20 -0300

    selection sort traditional algorithm

---
+++ Algorithms/Sorting/Selection Sort/selectionSort1.js
@@ -0,0 +1,42 @@
+function selectionSort(array){
+  //let min = array[0];
+  //let size = 
+  
+  for(let i = 0; i < array.length - 1; ++i){
+    let min = i;
+    for(let j = i + 1; j < array.length; ++j){
+      if(array[j] < array[min]){
+        min = j;
+      }
+    }
+    [array[min], array[i]] = [array[i], array[min]];
+  }
+  return array;
+}
+//TEST CASES:
+//1-:
+let testArray1 = [9,85,-5,10,0,-17,123,3,67];
+
+// console.time('Execution Time');
+let orderedArray1 = selectionSort(testArray1);
+// console.timeEnd('Execution Time');
+
+console.log(orderedArray1);
+
+//2-:
+let testArray2 = [593,12,0,-45,1,45,23,-435,-1];
+
+// console.time('Execution Time');
+let orderedArray2 = selectionSort(testArray2);
+// console.timeEnd('Execution Time');
+
+// console.log(orderedArray2);
+
+//3-:
+let testArray3 = [722,345,299,122,119,90,88,51,23,1,0,-1,-24,-121,-987];
+
+// console.time('Execution Time');
+let orderedArray3 = selectionSort(testArray3);
+// console.timeEnd('Execution Time');
+
+// console.log(orderedArray3);
