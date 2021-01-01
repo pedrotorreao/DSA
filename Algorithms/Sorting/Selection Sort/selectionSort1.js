665bc2e2be1e7a2be382064b41732362ae30708a
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2021-01-01 16:56:02 -0300

    adding comments

--- Algorithms/Sorting/Selection Sort/selectionSort1.js
+++ Algorithms/Sorting/Selection Sort/selectionSort1.js
@@ -1,7 +1,15 @@
+/****************************************************************/
+/*Selection Sort  ********/
+/****************************************************************/
+/*
+The selection sort algorithm sorts an array by repeatedly finding the minimum element (considering ascending order) 
+from unsorted part and putting it at the beginning. The algorithm maintains two subarrays in a given array.
+1) The subarray which is already sorted.
+2) Remaining subarray which is unsorted.
+In every iteration of selection sort, the minimum element (considering ascending order) from the unsorted subarray 
+is picked and moved to the sorted subarray. 
+*/
 function selectionSort(array){
-  //let min = array[0];
-  //let size = 
-  
   for(let i = 0; i < array.length - 1; ++i){
     let min = i;
     for(let j = i + 1; j < array.length; ++j){
