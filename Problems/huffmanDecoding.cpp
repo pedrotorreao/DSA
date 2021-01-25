73f72c6c597b084d78b2a952f449794955486ff0
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2021-01-25 07:00:08 -0300

    new solution (HR)

--- Problems/huffmanDecoding.cpp
+++ Problems/huffmanDecoding.cpp
@@ -26,7 +26,7 @@ need to print the decoded string.
 Output the decoded string on a single line.
 */
 /* 
-The structure of the node is
+The structure of the node is:
 
 typedef struct node
 {
@@ -38,7 +38,133 @@ typedef struct node
 }node;
 
 */
-
+// ---------- SOLUTION -----------
 void decode_huff(node *root, string s)
 {
+    string decoded{};
+    node *currentNode = root;
+
+    for (auto charac : s)
+    {
+        if (charac == '0')
+        {
+            currentNode = currentNode->left;
+            if (!currentNode->left && !currentNode->right)
+            {
+                decoded += currentNode->data;
+                currentNode = root;
+            }
+        }
+        else
+        {
+            currentNode = currentNode->right;
+            if (!currentNode->right && !currentNode->left)
+            {
+                decoded += currentNode->data;
+                currentNode = root;
+            }
+        }
+    }
+    cout << decoded;
 }
+// ---------------------------------
+// ------ Code provided by HR ------
+/*
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef struct node {
+    
+  	int freq;
+    char data;
+    node * left;
+    node * right;
+    
+} node;
+
+struct deref:public binary_function<node*, node*, bool> {
+  	bool operator()(const node * a, const node * b)const {
+        return a->freq > b->freq;
+    }
+};
+
+typedef priority_queue<node *,vector<node*>, deref> spq;
+
+node * huffman_hidden(string s) {
+
+    spq pq;
+    vector<int>count(256, 0);
+  
+    for(int i = 0; i < s.length(); i++) {
+        count[s[i]]++;
+    }
+    
+    for(int i = 0; i < 256; i++) {
+        
+        node * n_node = new node;
+        n_node->left = NULL;
+        n_node->right = NULL;
+        n_node->data = (char)i;
+        n_node->freq = count[i];
+      
+        if( count[i] != 0 )
+        	pq.push(n_node);
+      
+    }
+    
+    while( pq.size() != 1 ) {
+      
+        node * left = pq.top();
+        pq.pop();
+        node * right = pq.top();
+        pq.pop();
+        node * comb = new node;
+        comb->freq = left->freq + right->freq;
+        comb->data = '\0';
+        comb->left = left;
+        comb->right = right;
+        pq.push(comb);
+      
+    }
+    
+    return pq.top();
+    
+}
+
+void print_codes_hidden(node * root, string code, map<char, string>&mp) {
+    
+  	if(root == NULL)
+        return;
+    if(root->data != '\0') {
+        mp[root->data] = code;
+    }
+  
+    print_codes_hidden( root->left, code+'0', mp );
+    print_codes_hidden( root->right, code+'1', mp );
+    
+}
+
+----> SOLUTION <-----
+
+int main() {
+    
+  	string s;
+    std::cin >> s;
+  
+    node * tree = huffman_hidden(s);
+    string code = "";
+  
+    map<char, string> mp;
+    print_codes_hidden(tree, code, mp);
+    
+    string coded;
+  
+    for(int i = 0; i < s.length(); i++) {
+        coded += mp[s[i]];
+    }
+    
+    decode_huff(tree, coded);
+  
+    return 0;
+}
+*/
