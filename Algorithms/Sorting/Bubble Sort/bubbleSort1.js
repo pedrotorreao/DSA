b4c248c11a8551419771406256ce45cff53478e2
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2021-01-27 06:23:06 -0300

    add brief summary

--- Algorithms/Sorting/Bubble Sort/bubbleSort1.js
+++ Algorithms/Sorting/Bubble Sort/bubbleSort1.js
@@ -1,6 +1,18 @@
 /****************************************************************/
 /*Problem: Bubble Sort - Traditional Algorithm ********/
 /****************************************************************/
+/*
+Bubble sort, sometimes referred to as sinking sort, is a simple sorting algorithm that repeatedly steps through the list, 
+compares adjacent elements and swaps them if they are in the wrong order. The pass through the list is repeated until the 
+list is sorted. The algorithm, which is a comparison sort, is named for the way smaller or larger elements "bubble" to 
+the top of the list. This simple algorithm performs poorly in real world use and is used primarily as an educational tool. 
+More efficient algorithms such as quicksort, timsort, or merge sort are used by the sorting libraries built into popular 
+programming languages such as Python and Java. [Wikipedia]
+
+Time complexity: O(n^2)
+Space complexity: O(1)
+*/
+
 function bubbleSort(array){
   let sizing = array.length;
   for(let i = 0; i < sizing - 1; ++i){
