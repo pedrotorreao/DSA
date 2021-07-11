95510f99df4d421b0dcc8f5122e4cebe4927c276
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2021-07-11 02:05:17 -0300

    [update] working solution

--- Problems/src/tictactoeWinner.cpp
+++ Problems/src/tictactoeWinner.cpp
@@ -29,10 +29,10 @@ string tictactoe(vector<vector<int>> &moves)
     return "Pending";
   }
 
-  int rows = 3, cols = 3;
-  vector<vector<string>> mat(rows, vector<string>(cols, "-"));
-  unordered_map<int, int> matMap;
+  int size = 3;
+  vector<vector<string>> mat(size, vector<string>(size, "-"));
 
+  // fill the board:
   for (int i{0}; i < moves.size(); i++)
   {
     int rowNumber = moves.at(i).at(0);
@@ -47,6 +47,45 @@ string tictactoe(vector<vector<int>> &moves)
       mat.at(rowNumber).at(colNumber) = "O";
     }
   }
+
+  for (size_t i{0}; i < size; i++)
+  {
+    // check columns:
+    if (mat[i][0] == mat[i][1] && mat[i][0] == mat[i][2])
+    {
+      if (mat[i][0] != "-")
+      {
+        return (mat.at(i).at(0) == "X" ? "A" : "B");
+      }
+    }
+    // check rows:
+    if (mat[0][i] == mat[1][i] && mat[0][i] == mat[2][i])
+    {
+      if (mat[0][i] != "-")
+      {
+        return (mat.at(0).at(i) == "X" ? "A" : "B");
+      }
+    }
+  }
+
+  // check diagonals:
+  if ((mat[0][0] == mat[1][1] && mat[0][0] == mat[2][2]) || (mat[2][0] == mat[1][1] && mat[2][0] == mat[0][2]))
+  {
+    if (mat[1][1] != "-")
+    {
+      return (mat[1][1] == "X" ? "A" : "B");
+    }
+  }
+
+  // a tic tac toe game has at most 9 possible moves, if no solution was found
+  // and there are moves left, return "Pending":
+  if (moves.size() < 9)
+  {
+    return "Pending";
+  }
+
+  // otherwise, if no solution was found and there are no movements left, it's a draw:
+  return "Draw";
 }
 
 int main()
@@ -62,6 +101,12 @@ int main()
 
   vector<vector<int>> moves4 = {{0, 0}, {1, 1}};
   cout << "Result of game 4: " << tictactoe(moves4) << "\n";
+
+  vector<vector<int>> moves5 = {{0, 2}, {0, 1}, {2, 1}};
+  cout << "Result of game 5: " << tictactoe(moves5) << "\n";
+
+  vector<vector<int>> moves6 = {{0, 0}, {1, 1}, {0, 1}, {0, 2}, {1, 0}, {2, 0}};
+  cout << "Result of game 6: " << tictactoe(moves6) << "\n";
 
   return 0;
 }
