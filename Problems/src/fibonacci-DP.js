a3211722c428b73f6742e4d51b7234354d9b827a
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2021-07-26 22:08:07 -0300

    adding bottom up DP approach

--- Problems/src/fibonacci-DP.js
+++ Problems/src/fibonacci-DP.js
@@ -2,7 +2,8 @@
 /* Problem: Calculate the Fibonacci sequence of n - Dynamic Programming Approach   *******/
 /********************************************************************************************************/
 
-function fibonacci() {
+// ---- Top Down Approach - Memoization ----:
+function fibonacciTopDown() {
   let cache = {};
   return function f(n) {
     if (cache[n]) {
@@ -17,13 +18,27 @@ function fibonacci() {
   };
 }
 
-let fib = fibonacci();
+// ---- Bottom Up Approach - Tabulation ----:
+function fibonacciBottomUp(n) {
+  let res = [];
+  // push first 2 results:
+  res.push(0); // fib(0)
+  res.push(1); // fib(1)
 
-console.log(fib(10));
-console.log(fib(9));
-console.log(fib(8));
-console.log(fib(7));
-console.log(fib(6));
+  for (let i = 2; i <= n; i++) {
+    res.push(res[i - 1] + res[i - 2]);
+  }
+
+  return res.pop();
+}
+
+let fibTD = fibonacciTopDown();
+
+console.log(fibTD(10), fibonacciBottomUp(10));
+console.log(fibTD(9), fibonacciBottomUp(9));
+console.log(fibTD(8), fibonacciBottomUp(8));
+console.log(fibTD(7), fibonacciBottomUp(7));
+console.log(fibTD(6), fibonacciBottomUp(6));
 
 /*
 n: 0    1    2    3    4    5    6    7    8    9    10
