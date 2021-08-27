02cd26e8c63c1904d54b0cef0f55004eb1f44e37
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2021-08-26 22:22:07 -0300

    [update] add explanatation and run time analysis

--- Problems/src/isUnique.cpp
+++ Problems/src/isUnique.cpp
@@ -3,8 +3,20 @@
 /*********************************************************************************************/
 /*
 --Problem statement:
-Implement an algorithm to determine if a string has all unique characters. What if you cannot
-use additional data structures?
+  Implement an algorithm to determine if a string has all unique characters. What if you cannot
+  use additional data structures?
+--Reasoning:
+  First, we sort the string using C++ STL's std::sort() function and after we iterate through
+  the sorted string comparing adjacent characters to check if there is repetition. This will
+  run in O(n*log n) time, since the sorting is guaranteed to be O(n*log n) in the worst case
+  and the comparisons will run in linear time.
+
+  If using an additional data structure was allowed, a better approach would be to use a hash 
+  table to map the string characters as we iterate through the string, counting the characters 
+  as we go (e.g. map[charac] += 1) and as soon as a character is repeated we'd return false or
+  true if there's no repetition. This would have a linear running time, O(n).
+--Time complexity: O(n * log n)
+--Space complexity: O(1), no additional space is taken.
 */
 
 #include <iostream>
