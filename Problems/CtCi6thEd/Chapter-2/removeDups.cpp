3cff6369e89a7c98d586b5cab28079a64bfb9455
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2021-09-15 21:31:54 -0300

    [update] reorg. folders

---
+++ Problems/CtCi6thEd/Chapter-2/removeDups.cpp
@@ -0,0 +1,29 @@
+/*********************************************************************************************/
+/* Problem: Remove Duplicates (CtCi 2.1) ********/
+/*********************************************************************************************/
+/*
+--Problem statement:
+  Write code to remove duplicates from an unsorted linked list. FOLLOW UP: How would you solve
+  this problem if a temporary buffer is not allowed?
+
+--Reasoning:
+
+--Time complexity: 
+
+--Space complexity:
+*/
+
+#include <iostream>
+#include <list>
+
+void removeDups(std::list<int> &ll)
+{
+  // .. code
+}
+
+int main()
+{
+  // ..code
+
+  return 0;
+}
--- Problems/Others/firstRecurringCharacter.js
+++ Problems/Others/firstRecurringCharacter.js
@@ -1,7 +1,7 @@
 /*************************************************************************************************************/
 /*Problem: First Recurring Character - Similar to the 'Contains Duplicate' problem solved previously  ********/
 /*************************************************************************************************************/
-/*
+
 // BETTER SOLUTION: TIME COMPLEXITY O(N)
 function firstRecurringCharacter(nums) {
   let map = {};
@@ -9,8 +9,8 @@ function firstRecurringCharacter(nums) {
   //If the element was never seen, we insert it to the object with a value 'true', to indicate
   //that it has been seen. In case the element had already been seen, we return 'true'. If at the
   //end of the loop we don't see the same element twice, return 'false'.
-  for(let i = 0; i < nums.length; i++) {
-    if(!map[nums[i]]) {
+  for (let i = 0; i < nums.length; i++) {
+    if (!map[nums[i]]) {
       const item = nums[i];
       map[item] = true;
       //console.log(map);
@@ -20,17 +20,16 @@ function firstRecurringCharacter(nums) {
     }
   }
   //return false;
-  return 'undefined';
+  return "undefined";
 }
 
-let array1 = [1,3,4,5,6,0];
+let array1 = [1, 3, 4, 5, 6, 0];
 console.log(firstRecurringCharacter(array1));
-let array2 = [2,5,1,2,3,5,1,2,4];
+let array2 = [2, 5, 1, 2, 3, 5, 1, 2, 4];
 console.log(firstRecurringCharacter(array2));
-let array3 = [2,1,1,2,3,5,1,2,4];
+let array3 = [2, 1, 1, 2, 3, 5, 1, 2, 4];
 console.log(firstRecurringCharacter(array3));
-let array4 = [2,3,4,5];
+let array4 = [2, 3, 4, 5];
 console.log(firstRecurringCharacter(array4));
-let array5 = [2,5,5,2,3,5,1,2,4];
+let array5 = [2, 5, 5, 2, 3, 5, 1, 2, 4];
 console.log(firstRecurringCharacter(array5));
-*/
