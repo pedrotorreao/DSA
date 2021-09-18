55a881b0ab0f1ff7c6d90cae75be9fdde3f47234
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2021-09-17 22:30:58 -0300

    [add] CtCi 2.1 solution

--- Problems/CtCi6thEd/Chapter-2/removeDups.cpp
+++ Problems/CtCi6thEd/Chapter-2/removeDups.cpp
@@ -7,23 +7,65 @@
   this problem if a temporary buffer is not allowed?
 
 --Reasoning:
+  Iterate through the list mapping the elements into the hash table, once an element is seen 
+  again, set the previous node' next pointer to the current node' next node.
 
---Time complexity: 
+--Time complexity: O(N), where N is the length of the linked list.
 
---Space complexity:
+--Space complexity: O(N), due to the space used to map the linked list elements.
 */
 
 #include <iostream>
 #include <list>
+#include <unordered_map>
+#include "singlyLinkedList/singlyLinkedList.h"
 
-void removeDups(std::list<int> &ll)
+void removeDups(SinglyLinkedList &sll)
 {
-  // .. code
+  std::unordered_map<int, bool> map_duplicates;
+
+  Node *current_node = sll.getH();
+  Node *prev_node = nullptr;
+
+  while (current_node != nullptr)
+  {
+    if (!map_duplicates[current_node->data])
+    {
+      map_duplicates[current_node->data] = true;
+    }
+    else
+    {
+      prev_node->next = current_node->next;
+    }
+
+    prev_node = current_node;
+    current_node = current_node->next;
+  }
+
+  sll.decLength();
 }
 
 int main()
 {
-  // ..code
+  SinglyLinkedList sll;
+
+  sll.append(2);
+  sll.append(0);
+  sll.append(2);
+  sll.append(5);
+  sll.append(1);
+  sll.append(3);
+  sll.append(6);
+  sll.append(9);
+  sll.append(5);
+  sll.append(7);
+
+  std::cout << "--original linked list: ";
+  sll.display();
+
+  removeDups(sll);
+  std::cout << "--edited linked list: ";
+  sll.display();
 
   return 0;
 }
