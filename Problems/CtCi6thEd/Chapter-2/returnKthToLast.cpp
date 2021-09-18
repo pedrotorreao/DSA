936205d7ee0feb019d0fc897f6a6933a06216319
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2021-09-17 22:31:29 -0300

    [add] CtCi 2.2: setup file (WIP)

---
+++ Problems/CtCi6thEd/Chapter-2/returnKthToLast.cpp
@@ -0,0 +1,36 @@
+/*********************************************************************************************/
+/* Problem: Return Kth to Last (CtCi 2.2) ********/
+/*********************************************************************************************/
+/*
+--Problem statement:
+  Implement an algorithm to find the kth to last element of a singly linked list.
+
+--Reasoning:
+  
+
+--Time complexity: 
+
+--Space complexity: 
+*/
+
+#include <iostream>
+#include <list>
+#include "singlyLinkedList/singlyLinkedList.h"
+
+int returnKthToLast(SinglyLinkedList &sll)
+{
+  int kthToLastEl{};
+
+  // ..code
+
+  return kthToLastEl;
+}
+
+int main()
+{
+  SinglyLinkedList sll;
+
+  // .. code
+
+  return 0;
+}
