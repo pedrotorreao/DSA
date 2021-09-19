13ce322865a22e29cfa4aca08477ebe94b6ee502
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2021-09-18 23:18:12 -0300

    [update] working solution

--- Problems/CtCi6thEd/Chapter-2/returnKthToLast.cpp
+++ Problems/CtCi6thEd/Chapter-2/returnKthToLast.cpp
@@ -6,31 +6,54 @@
   Implement an algorithm to find the kth to last element of a singly linked list.
 
 --Reasoning:
-  
+  Since we track the size of the list as we append elements, it is just a matter 
+  of iterating through the list until we get to the (list_size - k)th node and 
+  return its value.
 
---Time complexity: 
+--Time complexity: O(N), where N is the size of the list.
 
---Space complexity: 
+--Space complexity: O(1), no extra space needed.
 */
 
 #include <iostream>
-#include <list>
+#include <limits.h>
 #include "singlyLinkedList/singlyLinkedList.h"
 
-int returnKthToLast(SinglyLinkedList &sll)
+int returnKthToLast(SinglyLinkedList &sll, const int k)
 {
-  int kthToLastEl{};
+  int ll_length{};
+  ll_length = sll.getLength();
 
-  // ..code
+  if (k > ll_length)
+  {
+    std::cerr << "k is greater than the size of the list, try again!\n";
+    return INT_MIN;
+  }
 
-  return kthToLastEl;
+  Node *current_node = sll.getH();
+
+  for (int i{1}; i < (ll_length - k); i++)
+  {
+    current_node = current_node->next;
+  }
+
+  return current_node->data;
 }
 
 int main()
 {
   SinglyLinkedList sll;
+  int k{0};
 
-  // .. code
+  sll.feedList(10, 1, 10);
+
+  std::cout << "--linked list: ";
+  sll.display();
+
+  std::cout << "Enter the distance to last element (k): ";
+  std::cin >> k;
+
+  std::cout << "--kth element to last: " << returnKthToLast(sll, k) << "\n";
 
   return 0;
 }
