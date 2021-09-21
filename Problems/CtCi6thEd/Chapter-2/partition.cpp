6f128497225c92c6444434f07b4a6337651d813c
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2021-09-20 21:19:04 -0300

    [update] working solution

--- Problems/CtCi6thEd/Chapter-2/partition.cpp
+++ Problems/CtCi6thEd/Chapter-2/partition.cpp
@@ -11,18 +11,82 @@
     Output: 3->1->2->10->5->5->8
 
 --Reasoning:
-
---Time complexity: 
-
---Space complexity: 
+  The approach may not be the cleanest but it's quite simple. It consists of using two arrays
+  in order to keep track of the elements smaller and greater than x. Also, a counter keeps
+  track of how many times x appears in the linked list, if any. After a first iteration through
+  the list, we'll have the exact count for each of these cases. Now it's just a matter of 
+  iterating through the list a second time replacing the current node data by the arrays elements
+  beginning by the smaller, then by x and finally by the greater until they are all empty and the
+  count is zero.
+--Time complexity: O(n), n being the linked list length.
+ 
+--Space complexity: O(n), in the worst case where we have only smaller/greater than x elements.
 */
 
 #include <iostream>
+#include <vector>
 #include "singlyLinkedList/singlyLinkedList.h"
 
 void partition(Node *ll_head, int x)
 {
-  //..code
+  if (ll_head == nullptr)
+  {
+    return;
+  }
+
+  Node *current_node = ll_head;
+  std::vector<int> smaller{}; // store ll elements smaller than x
+  std::vector<int> greater{}; // store ll elements greater than x
+  int x_count{0}, ll_size{0};
+
+  while (current_node != nullptr)
+  {
+    ll_size++; // count list elements
+
+    if (current_node->data < x)
+    {
+      smaller.push_back(current_node->data);
+    }
+    else if (current_node->data > x)
+    {
+      greater.push_back(current_node->data);
+    }
+    else
+    {
+      x_count++; // count x frequency
+    }
+
+    current_node = current_node->next;
+  }
+
+  // if there are only elementes smaller than or greater than x in the
+  // list, there is no partition to be done:
+  if ((smaller.size() == ll_size) || (greater.size() == ll_size))
+  {
+    return;
+  }
+
+  current_node = ll_head; // restart pointer
+
+  while (current_node != nullptr)
+  {
+    if (smaller.size())
+    { // replace current data by the smaller elements
+      current_node->data = smaller.back();
+      smaller.pop_back();
+    }
+    else if (x_count)
+    { // replace current data by x
+      current_node->data = x;
+      --x_count;
+    }
+    else if (greater.size())
+    { // replace current data by the greater elements
+      current_node->data = greater.back();
+      greater.pop_back();
+    }
+    current_node = current_node->next;
+  }
 }
 
 int main()
@@ -52,7 +116,7 @@ int main()
 
   partition(ll_head, x);
 
-  std::cout << "Partitioned linked list: ";
+  std::cout << "Modified linked list: ";
   sll.display();
 
   return 0;
