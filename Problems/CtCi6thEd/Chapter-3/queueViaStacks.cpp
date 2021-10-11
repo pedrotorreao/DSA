ce78075b4f4c16179dae7eca245614b183099c60
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2021-10-10 23:36:46 -0300

    problem: queue via stacks (WIP)

---
+++ Problems/CtCi6thEd/Chapter-3/queueViaStacks.cpp
@@ -0,0 +1,32 @@
+/*********************************************************************************************/
+/* Problem: Queue via Stacks (CtCi 3.4) ********/
+/*********************************************************************************************/
+/*
+--Problem statement:
+  Implement a MyQueue class which implements a queue using two stacks.
+
+--Reasoning:
+
+--Time complexity: 
+ 
+--Space complexity: 
+  
+*/
+
+#include <iostream>
+#include "stack/stack.h"
+
+class MyQueue
+{
+private:
+public:
+  MyQueue() {}
+  ~MyQueue() {}
+};
+
+int main()
+{
+  // .. code
+
+  return 0;
+}
