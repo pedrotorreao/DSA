eb6e9d060d3b19e2f78f048822110e9836e3328c
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2021-11-25 22:41:40 -0300

    add book solution

--- Problems/CtCi6thEd/Chapter-2/returnKthToLast.cpp
+++ Problems/CtCi6thEd/Chapter-2/returnKthToLast.cpp
@@ -6,8 +6,8 @@
   Implement an algorithm to find the kth to last element of a singly linked list.
 
 --Reasoning:
-  Since we track the size of the list as we append elements, it is just a matter 
-  of iterating through the list until we get to the (list_size - k)th node and 
+  Since we track the size of the list as we append elements, it is just a matter
+  of iterating through the list until we get to the (list_size - k)th node and
   return its value.
 
 --Time complexity: O(N), where N is the size of the list.
@@ -19,7 +19,7 @@
 #include <limits.h>
 #include "singlyLinkedList/singlyLinkedList.h"
 
-int returnKthToLast(SinglyLinkedList &sll, const int k)
+int returnKthToLast_1(SinglyLinkedList &sll, const int k)
 {
   int ll_length{};
   ll_length = sll.getLength();
@@ -40,6 +40,34 @@ int returnKthToLast(SinglyLinkedList &sll, const int k)
   return current_node->data;
 }
 
+// CtCi Solution #3:
+int returnKthToLast_2(SinglyLinkedList &sll, const int k)
+{
+  Node *p1 = sll.getH();
+  Node *p2 = sll.getH();
+
+  // Move p1 k nodes into the list:
+  for (int i{0}; i <= k; ++i)
+  {
+    if (p1 == nullptr)
+    {
+      p1 = new Node(INT_MIN);
+      return p1->data;
+    }
+    p1 = p1->next;
+  }
+
+  // Move them at the same pace. When p1 gets hits the end,
+  // p2 will be at the rigth element.
+  while (p1 != nullptr)
+  {
+    p1 = p1->next;
+    p2 = p2->next;
+  }
+
+  return p2->data;
+}
+
 int main()
 {
   SinglyLinkedList sll;
@@ -53,7 +81,8 @@ int main()
   std::cout << "Enter the distance to last element (k): ";
   std::cin >> k;
 
-  std::cout << "--kth element to last: " << returnKthToLast(sll, k) << "\n";
+  std::cout << "--kth element to last: " << returnKthToLast_1(sll, k) << "\n";
+  std::cout << "--kth element to last: " << returnKthToLast_2(sll, k) << "\n";
 
   return 0;
 }
