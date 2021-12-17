36854a2a22d3a5fe4bb252a40dff9943b9858a5e
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2021-12-17 02:21:03 -0300

    dynamic array class [needs minor fixes]

--- DataStructures/Array/DynamicArray/dynamicArray.h
+++ DataStructures/Array/DynamicArray/dynamicArray.h
@@ -12,64 +12,64 @@ private:
   int size;
   int total_capacity;
   int growth_factor;
-  std::unique_ptr<T> ptr;
+  // std::unique_ptr<T> ptr;
+  T *arr;
 
 public:
   /* ------------ Modifiers ------------ */
   // adds new element to the end of the array:
   void push(T value)
   {
-    if (this->get_size() == this->total_capacity)
-    {
-      this->resize();
-    }
-    ++this->size;
-    this->ptr[size] = value;
+    this->insert(value, this->get_size());
   }
   // pops element at the end of the array:
-  T pop(void)
+  void pop(void)
   {
-    --this->size;
-    if (this->get_size() < (this->total_capacity / 2))
-    {
-      this->resize();
-    }
+    this->remove(this->get_size() - 1);
   }
   // insert new element at a given position:
   void insert(T value, int pos)
   {
-    if (pos == this->get_size() - 1)
+    if (this->get_size() == this->total_capacity)
     {
-      this->push(value);
+      resize();
     }
-    else
+
+    for (int i = this->size; i > pos; --i)
     {
-      if (this->get_size() == this->total_capacity)
-      {
-        resize();
-      }
-
-      for (int i = this->size; i > pos; --i)
-      {
-        this->ptr[i] == this->ptr[i - 1];
-      }
-
-      this->ptr[pos] = value;
-      this->size++;
+      this->arr[i] == this->arr[i - 1];
     }
+
+    this->size++;
+    this->arr[pos] = value;
   }
   // removes element at a given position:
   void remove(int pos)
   {
-    this->size--;
+    assert(pos >= 0 && pos < this->get_size());
+
     for (int i = pos; i < this->size; i++)
     {
+      arr[i] = arr[i + 1];
     }
+
+    this->size--;
   }
   // resizes array to actual size:
   void resize(void)
   {
+    this->total_capacity *= this->growth_factor;
+
+    T *temp = new T[this->total_capacity];
+
+    std::copy(arr, arr + this->get_size(), temp);
+
+    delete[] arr;
+
+    arr = temp;
   }
+
+  // Other methods that could be added: clear() and shrink()
 
   /* ------------ Getters ------------ */
   int get_size(void) const
@@ -86,14 +86,14 @@ public:
   {
     assert(this->get_size() > 0);
 
-    return ptr[0];
+    return arr[0];
   }
 
   T back(void) const
   {
     assert(this->get_size() > 0);
 
-    return ptr[this->size - 1];
+    return arr[this->size - 1];
   }
 
   bool isEmpty(void) const
@@ -107,21 +107,38 @@ public:
   {
     assert(pos < this->get_size() && pos >= 0);
 
-    return ptr[pos];
+    return arr[pos];
   }
 
   // display:
-  void print(void);
+  void print(void)
+  {
+    if (this->isEmpty())
+    {
+      std::cout << "Array is currently empty.\n";
+      return;
+    }
+
+    std::cout << "Array elements: ";
+    for (int i{0}; i < this->get_size(); ++i)
+    {
+      std::cout << arr[i] << "   ";
+    }
+    std::cout << "\n";
+  }
 
   /* ------------ Overloaded constructor ------------- */
-  DynamicArray(int capacity, int growth = 2)
+  DynamicArray(int capacity = 5, int growth = 2)
   {
     this->size = 0;
     this->total_capacity = capacity;
     this->growth_factor = growth;
 
-    ptr = new T[total_capacity];
+    arr = new T[total_capacity];
   }
   /* ------------ Destructor ------------- */
-  ~DynamicArray() = default;
+  ~DynamicArray()
+  {
+    delete[] arr;
+  }
 };
