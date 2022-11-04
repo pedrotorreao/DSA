b4495090df9e9835cde0af1ffdbd979271dc2274
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2022-11-04 00:09:24 -0300

    solution: LC 238 [improved approach 2]

--- Problems/Leetcode/productExceptSelf.cpp
+++ Problems/Leetcode/productExceptSelf.cpp
@@ -28,40 +28,68 @@ Problem: LC 238. Product of Array Except Self
     :: -30 <= nums[i] <= 30
     :: The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
 
---Reasoning:
-
---Time complexity:
-
---Space complexity:
-
 Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does
 not count as extra space for space complexity analysis.)
 
 */
 
+#include <algorithm>
 #include <cmath>
 #include <iostream>
 #include <vector>
 
 void display1D(const std::vector<int> &arr);
 
+std::vector<int> productExceptSelf_1(std::vector<int> &nums);
+
 /*
-  # Approach 1 - Naive Approach:
-    --Reasoning: Use nested loops.
-    --Time Complexity: O(N^2)
+  # Approach 2:
+    --Reasoning:
+      First, we calculate the total product. Then, we iterate over 'nums[]' filling 'ans[]' with
+      total_product divided by self. We only need to deal with the special cases in which 'nums[i]=0'.
+      For these cases, we know the following:
+          [I]. If there are more than 1 zero, then no matter which element is excluded during the product,
+        there will always be a product by zero, so all the elements in the output array are zero.
+          [II]. If there is only 1 zero in the array, all the elements in the output array will be zero,
+        except for the position where the zero is present in nums[], since all the other elements except
+        itself will be multiplied by this zero.
+          [III]. If there are no zeros, then the element at position 'i' in the output array, 'ans[]', will
+        be the total product divided by self, i.e.:
+              ans[i] = total_product / nums[i].
+      This approach is an improvement over the "# Approach 1" since there are no nested loops involved;
+      however, it's not a valid solution to the problem according to the problem description since it
+      uses the division operator.
+
+    --Time Complexity: O(N)
+
     --Space Complexity: O(1)
 */
 std::vector<int> productExceptSelf(std::vector<int> &nums) {
-  const int N = nums.size();
-  std::vector<int> ans(N, 1);
+  const int SIZE = nums.size();
 
-  for (int i{0}; i < N; ++i) {
-    for (int j{0}; j < N; ++j) {
-      if (i == j)
-        continue;
+  // count zeros:
+  int zeros = std::count(nums.begin(), nums.end(), 0);
 
-      ans.at(i) = ans.at(i) * nums.at(j);
+  // [I] we have more than 1 zero present in nums[]:
+  if (zeros > 1)
+    return std::vector<int>(SIZE, 0);
+
+  // calculate total product:
+  int total_product{1};
+  for (const auto &n : nums)
+    if (n)
+      total_product *= n;
+
+  std::vector<int> ans(SIZE, 1);
+
+  for (int i{0}; i < SIZE; ++i) {
+    // [II] there is only 1 zero:
+    if (zeros) {
+      ans.at(i) = (nums.at(i) != 0) ? 0 : total_product;
+      continue;
     }
+    // [III] there are no zeros:
+    ans.at(i) = total_product / nums.at(i);
   }
 
   return ans;
@@ -107,6 +135,30 @@ int main() {
   display1D(res);
 
   return 0;
+}
+
+/*
+  # Approach 1 - Naive Approach:
+    --Reasoning: Use nested loops.
+
+    --Time Complexity: O(N^2)
+
+    --Space Complexity: O(1)
+*/
+std::vector<int> productExceptSelf_1(std::vector<int> &nums) {
+  const int N = nums.size();
+  std::vector<int> ans(N, 1);
+
+  for (int i{0}; i < N; ++i) {
+    for (int j{0}; j < N; ++j) {
+      if (i == j)
+        continue;
+
+      ans.at(i) = ans.at(i) * nums.at(j);
+    }
+  }
+
+  return ans;
 }
 
 void display1D(const std::vector<int> &arr) {
