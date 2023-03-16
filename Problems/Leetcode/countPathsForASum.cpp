96df24919a07239a77908a758845f43ee9e55c2f
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2023-03-15 22:19:19 -0300

    solution: LC 129

--- Problems/Leetcode/countPathsForASum.cpp
+++ Problems/Leetcode/countPathsForASum.cpp
@@ -1,6 +1,38 @@
+/****************************************************************************************/
+/* Problem: 129. Sum Root to Leaf Numbers  **********************************************/
+/****************************************************************************************/
+/*
+--Problem statement:
+  Given the root of a binary tree and an integer targetSum, return the number of paths where
+  the sum of the values along the path equals targetSum.
+
+  The path does not need to start or end at the root or a leaf, but it must go downwards
+  (i.e., traveling only from parent nodes to child nodes).
+
+  > Example 1:
+      Input: root = [10,5,-3,3,2,null,11,3,-2,null,1], targetSum = 8
+      Output: 3
+      Explanation: The paths that sum to 8 are shown.
+
+  > Example 2:
+      Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+      Output: 3
+
+--Inputs:
+  -TreeNode*: root of a binary tree.
+
+--Output:
+  -int: number of paths where the sum of the values along the path equals targetSum
+
+--Constraints:
+  :: The number of nodes in the tree is in the range [0, 1000].
+  :: -109 <= Node.val <= 109
+  :: -1000 <= targetSum <= 1000
+
+--Reasoning: See comments below.
+*/
+
 #include <iostream>
-#include <queue>
-#include <stack>
 #include <unordered_map>
 #include <utility>
 #include <vector>
@@ -75,16 +107,25 @@ private:
   }
 };
 
+/*
+--Reasoning:
+  In the previous solution, for each node we are repeating the traversal for the current path and analysing if the current sub-path ending at our current node matches the target sum. A good improvement is to use the Prefix Sum technique to efficiently manage the path sums. See comments below.
+--Time complexity:
+  Worst case: O(N), where N is the total number of nodes in the tree.
+--Space complexity:
+  O(N), where N is the total number of nodes in the tree. This is due to the storage
+  required for the recursion stack, as well as the prefix sum.
+*/
 class Solution_2 {
 public:
   int countPaths_2(TreeNode *root, int target) {
-    std::unordered_map<int, int> map; // store the prefix sum' frequency
+    std::unordered_map<long, int> map; // store the prefix sum' frequency
 
     return countPaths_DFS_2(root, target, map, 0);
   }
 
 private:
-  int countPaths_DFS_2(TreeNode *node, int target, std::unordered_map<int, int> &map, int currPathSum) {
+  int countPaths_DFS_2(TreeNode *node, int target, std::unordered_map<long, int> &map, long currPathSum) {
     if (node == nullptr)
       return 0;
 
