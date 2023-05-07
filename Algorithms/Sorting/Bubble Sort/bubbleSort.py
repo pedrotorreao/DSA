c877cf236e24a8dfcc7d209a8d135c4486b3a971
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2023-05-06 22:21:28 -0300

    update: add a improved version

--- Algorithms/Sorting/Bubble Sort/bubbleSort.py
+++ Algorithms/Sorting/Bubble Sort/bubbleSort.py
@@ -1,4 +1,4 @@
-def bubble_sort(nums):
+def bubble_sort_traditional(nums):
     n = len(nums)
     swapping = True
 
@@ -12,11 +12,27 @@ def bubble_sort(nums):
 
     return nums
 
+def bubble_sort_improved(nums):
+	n = len(nums)
+	while n > 1:
+		newn = 0
+		for i in range(1, n):
+			if nums[i-1] > nums[i]:
+				nums[i], nums[i-1] = nums[i-1], nums[i]
+				newn = i
+		n = newn
+	
+	return nums
 
 def main():
     nums = [3, 19, 2, 0, -12, 56, 21, 43, 1]
     print(f"original array: {nums}\n")
-    bubble_sort(nums)
+    bubble_sort_traditional(nums)
+    print(f"sorted array: {nums}\n\n")
+
+    nums = [91,23,9,-3,6,-23,12,45]
+    print(f"original array: {nums}\n")
+    bubble_sort_traditional(nums)
     print(f"sorted array: {nums}\n\n")
 
 
