40e9815f8657c23c97252d27133e0a4614c842a1
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2024-01-14 21:14:28 -0300

    solution: LC 461

---
+++ Problems/Leetcode/hammingDistance.cpp
@@ -0,0 +1,74 @@
+/* * * * * * * * * * * * * * * * * *
+Problem: LC 461. Hamming Distance
+
+--Problem statement:
+The Hamming distance between two integers is the number of positions at which
+the corresponding bits are different.
+Given two integers x and y, return the Hamming distance between them.
+
+  > Example 1:
+      Input: x = 1, y = 4
+      Output: 2
+      Explanation:
+      1   (0 0 0 1)
+      4   (0 1 0 0)
+            ↑   ↑
+      The above arrows point to positions where the corresponding bits are different.
+
+  > Example 2:
+      Input: x = 3, y = 1
+      Output: 1
+
+--Constraints:
+    :: 0 <= x, y <= 2^31 - 1
+
+--Inputs:
+  - int: integers x and y
+
+--Outputs:
+  - int: hamming distance
+
+--Reasoning: Use bit manipulation. By XOR-ing the input integers, we get as result an integer for which
+the bit representation has 1's where the input numbers differ. Example:
+    x = 5 -> 0101
+    y = 2 -> 0010
+      x ^ y = 0101 ^ 0010 = 0111 = 7
+  After that, we just need to count the number of bits which are 1.
+
+--Time complexity:
+    :: O(log n), where n is the result of the XOR operation between the input integers.
+
+--Space complexity:
+    :: O(1), since no additional input-dependent space is allocated.
+
+* * * * * * * * * * * * * * * * * */
+
+#include <iostream>
+
+class Solution {
+public:
+  static int hammingDistance(int x, int y) {
+    int r = (x ^ y), diff = 0;
+
+    while (r) {
+      if (r & 1)
+        ++diff;
+
+      r /= 2;
+    }
+
+    return diff;
+  }
+};
+
+int main() {
+  int x = 0, y = 0;
+
+  x = 1;
+  y = 4;
+  std::cout << "The hamming distance between \"" << x << "\" and \"" << y << "\" is: \"" << Solution::hammingDistance(x, y) << "\"\n";
+
+  x = 5;
+  y = 2;
+  std::cout << "The hamming distance between \"" << x << "\" and \"" << y << "\" is: \"" << Solution::hammingDistance(x, y) << "\"\n";
+}
