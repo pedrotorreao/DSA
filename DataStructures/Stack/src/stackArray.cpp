371cb62c3e4299ea6942888bac930a78667b873e
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2024-06-11 07:02:29 -0300

    ds: improve stack array implementation

--- DataStructures/Stack/src/stackArray.cpp
+++ DataStructures/Stack/src/stackArray.cpp
@@ -4,113 +4,88 @@ DS: Stack using Arrays
 #include <iostream>
 #include <string>
 #include <vector>
-#include <stdexcept>
 
-class Stack
-{
+class Stack {
 private:
-  std::string top;
   std::vector<std::string> stack;
 
 public:
-  Stack();
-  void pop(void);
-  void push(std::string d);
-  bool isEmpty(void);
-  std::string peek(void);
-  int getSize(void);
-  ~Stack();
+  Stack() {}
+
+  bool pop(void) {
+    if (this->stack.empty())
+      return false;
+
+    this->stack.pop_back();
+    return true;
+  }
+
+  bool push(std::string val) {
+    if (val.empty())
+      return false;
+
+    this->stack.push_back(val);
+    return true;
+  }
+
+  bool isEmpty(void) {
+    return this->stack.empty();
+  }
+
+  std::string peek(void) {
+    if (this->stack.empty())
+      return "-- EMPTY STACK --";
+
+    return this->stack.back();
+  }
+
+  int getSize(void) {
+    return this->stack.size();
+  }
+
+  ~Stack() = default;
 };
 
-Stack::Stack()
-{
-  this->top = "";
-  this->stack = {};
-}
-
-void Stack::pop(void)
-{
-  if (this->top != "")
-  {
-    this->stack.pop_back();
-    this->top = this->stack.size() ? this->stack.at(this->stack.size() - 1) : "";
-    return;
-  }
-  throw std::length_error("stack is empty!");
-}
-
-void Stack::push(std::string d)
-{
-  if (d.size() > 0)
-  {
-    this->stack.push_back(d);
-    this->top = this->stack.at(this->stack.size() - 1);
-    return;
-  }
-  throw std::invalid_argument("invalid argument!");
-}
-
-bool Stack::isEmpty(void)
-{
-  return (this->stack.size() == 0);
-}
-
-std::string Stack::peek(void)
-{
-  if (this->top != "")
-  {
-    return this->top;
-  }
-  throw std::length_error("stack is empty!");
-}
-
-int Stack::getSize(void)
-{
-  return this->stack.size();
-}
-
-Stack::~Stack() {}
-
-int main()
-{
+int main() {
   std::cout.setf(std::ios::boolalpha);
 
   Stack stack;
+  bool success = false;
 
   // --test 1: try to pop from empty stack:
-  // stack.pop(); //should throw exception
+  // success = stack.pop(); //should throw exception
 
   // add elements:
-  stack.push("work");
-  stack.push("supermarket");
-  stack.push("shower");
-  stack.push("study");
+  success = stack.push("work");
+  success = stack.push("supermarket");
+  success = stack.push("shower");
+  success = stack.push("study");
 
   // --test 2: get top element:
-  std::cout << "Stack top: " << stack.peek() << "\n"; //should output "study session"
+  std::cout << "Stack top: " << stack.peek() << "\n"; // "study"
   std::cout << "---\n";
 
   // --test 3: get stack size:
-  std::cout << "Stack size: " << stack.getSize() << "\n"; // should output 4
+  std::cout << "Stack size: " << stack.getSize() << "\n"; // 4
   std::cout << "---\n";
 
   // --test 4: pop top element:
   stack.pop();
-  std::cout << "Stack top: " << stack.peek() << "\n";     //should output "shower"
-  std::cout << "Stack size: " << stack.getSize() << "\n"; // should output 3
+  std::cout << "Stack top: " << stack.peek() << "\n";     // "shower"
+  std::cout << "Stack size: " << stack.getSize() << "\n"; // 3
   std::cout << "---\n";
 
   // --test 5: check if stack is empty:
-  std::cout << "The stack is empty: " << stack.isEmpty() << "\n"; // should output false
+  std::cout << "The stack is empty: " << stack.isEmpty() << "\n"; // false
   std::cout << "---\n";
 
   // --test 6: pop all elements:
-  stack.pop();
-  stack.pop();
-  stack.pop();
-  //std::cout << "Stack top: " << stack.peek() << "\n";             //should throw exception
-  std::cout << "Stack size: " << stack.getSize() << "\n";         // should output 0
-  std::cout << "The stack is empty: " << stack.isEmpty() << "\n"; // should output true
+  success = stack.pop();
+  success = stack.pop();
+  success = stack.pop();
+  std::cout << "Stack top: " << stack.peek() << "\n";             // "-- EMPTY STACK --"
+  std::cout << "Stack size: " << stack.getSize() << "\n";         // 0
+  std::cout << "The stack is empty: " << stack.isEmpty() << "\n"; // true
   std::cout << "---\n";
 
   return 0;
