0246ea925cd6284297eff84a98eff362ac36d6f0
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2024-06-27 20:38:49 -0300

    update: minor changes

--- DataStructures/Tries/trie.cpp
+++ DataStructures/Tries/trie.cpp
@@ -1,31 +1,32 @@
-/****************************************************************/
-/* Tries C++ ********/
-/****************************************************************/
+/***
+Trie -- C++
+*/
 
 #include <iostream>
 #include <vector>
 
-class TrieNode
-{
+class TrieNode {
 public:
+  // array of pointers to 'TreeNode' (26: letters on the English alphabet):
   TrieNode *children[26];
+  // boolean used to indicate that the current TreeNode corresponds to the end of a word:
   bool endOfWord;
+  // counter which indicates how many words the current TreeNode is part of:
   int count;
 
-  TrieNode()
-  {
+  // constructor:
+  TrieNode() {
     endOfWord = false;
     count = 0;
 
-    for (size_t i{0}; i < 26; i++)
-    {
+    // initialize all pointers to null:
+    for (size_t i{0}; i < 26; i++) {
       children[i] = nullptr;
     }
   }
 };
 
-class Trie
-{
+class Trie {
 private:
   TrieNode *root;
 
@@ -38,58 +39,51 @@ public:
   bool hasDependent(TrieNode *node);
   int searchPrefix(std::string word);
 
-  Trie()
-  {
+  Trie() {
     this->root = new TrieNode();
   }
   ~Trie() {}
 };
 
-void Trie::insertWord(std::string word)
-{
+void Trie::insertWord(std::string word) {
   TrieNode *current = this->root;
 
-  for (auto i{0}; i < word.size(); i++)
-  {
-    // the position in the nodes vector is determined by subtracting the
-    // ascii code for the letter word[i] by the ascii code for 'a':
+  for (auto i{0}; i < word.size(); i++) {
+    // calculate the position in the 'children' array by subtracting the
+    // ascii code for the character word[i] by the ascii code for 'a':
     auto j = int(word.at(i)) - 97; // ascii('a') = 97
 
     // check if there is already a word which uses the letter word[i],
     // if not, initialize a new node at that position:
-    if (current->children[j] == nullptr)
-    {
+    if (current->children[j] == nullptr) {
       current->children[j] = new TrieNode();
     }
 
-    // increment the word count for node children[j] since a new word is being added which whis node is part of:
+    // increment the word count for node children[j] since a new word is being
+    // added which this node is a part of:
     ++current->children[j]->count;
 
     // advance to the next character in the word:
     current = current->children[j];
   }
-
+  // set the flag:
   current->endOfWord = true;
 }
 
-void Trie::deleteWord(std::string word)
-{
-  if (searchWord(word))
-  {
+void Trie::deleteWord(std::string word) {
+  // check if 'word' exists in the trie before trying to delete it:
+  if (searchWord(word)) {
     deleteHelper(word, this->root, 0);
   }
 }
 
-bool Trie::searchWord(std::string word)
-{
+bool Trie::searchWord(std::string word) {
   TrieNode *current = this->root;
 
-  for (auto i{0}; i < word.size(); i++)
-  {
+  for (auto i{0}; i < word.size(); i++) {
     auto j = int(word.at(i)) - 97;
 
-    if (current->children[j] == nullptr)
-    {
+    if (current->children[j] == nullptr) {
       return false;
     }
 
@@ -99,32 +93,33 @@ bool Trie::searchWord(std::string word)
   return (current->endOfWord);
 }
 
-bool Trie::deleteHelper(std::string word, TrieNode *parent, int idx)
-{
+bool Trie::deleteHelper(std::string word, TrieNode *parent, int idx) {
   char currentCharac = word.at(idx);
   TrieNode *current = parent->children[int(currentCharac) - 97];
   bool removable = false;
 
-  if (current->count && idx + 1 < word.size())
-  {
+  // case #1 - current character is a prefix and we have not reached the end of the word:
+  if (current->count && idx + 1 < word.size()) {
+    // recurse down the trie until we've traversed the whole 'word'.
     deleteHelper(word, current, idx + 1);
-
+    // decrease word count for the current character:
     parent->children[int(currentCharac) - 97]->count--;
-
+    // return false because the nodes itself cannot be deleted:
     return false;
   }
-
-  if (idx == word.size() - 1)
-  {
-    if (current->count)
-    {
+  // case #2 - we have reached the end of the word:
+  if (idx == word.size() - 1) {
+    // case #2.a - current character is a prefix, so the node itself cannot be deleted:
+    if (current->count) {
+      // decrease word count for the current character:
       parent->children[int(currentCharac) - 97]->count--;
+      // set the 'endOfWord' flag to false since the word ending at this character is being deleted:
       current->endOfWord = false;
-
+      // return false because the nodes itself cannot be deleted:
       return false;
     }
-    else
-    {
+    // case #2.b - current character is not a prefix, so the node can be safely deleted:
+    else {
       parent->children[int(currentCharac) - 97]->count--;
       current = nullptr;
 
@@ -132,16 +127,14 @@ bool Trie::deleteHelper(std::string word, TrieNode *parent, int idx)
     }
   }
 
-  if (current->endOfWord && idx + 1 < word.size())
-  {
+  if (current->endOfWord && idx + 1 < word.size()) {
     deleteHelper(word, current, idx + 1);
     return false;
   }
 
   removable = deleteHelper(word, current, idx + 1);
 
-  if (removable)
-  {
+  if (removable) {
     parent->children[int(currentCharac) - 97]->count--;
     current = nullptr;
   }
@@ -149,15 +142,12 @@ bool Trie::deleteHelper(std::string word, TrieNode *parent, int idx)
   return false;
 }
 
-int Trie::searchPrefix(std::string word)
-{
+int Trie::searchPrefix(std::string word) {
   TrieNode *current = this->root;
 
-  for (size_t i{0}; i < word.length(); i++)
-  {
+  for (size_t i{0}; i < word.length(); i++) {
     size_t j = int(word.at(i)) - 97;
-    if (current->children[j] == nullptr)
-    {
+    if (current->children[j] == nullptr) {
       return 0;
     }
     current = current->children[j];
@@ -165,8 +155,7 @@ int Trie::searchPrefix(std::string word)
   return current->count;
 }
 
-int main()
-{
+int main() {
   std::cout.setf(std::ios::boolalpha);
 
   Trie myTrie;
