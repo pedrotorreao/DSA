8279db56626ab742671739709d585d8694035f98
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2024-11-17 12:14:58 -0300

    add: template for solution

---
+++ Problems/Leetcode/codeChallengeTemplate.cpp
@@ -0,0 +1,30 @@
+/***
+Problem: LC <#id>. <title>
+(https://leetcode.com/problems/<problem-title>/description/)
+
+--Problem statement:
+
+
+--Inputs:
+
+--Output:
+
+--Constraints:
+
+--Reasoning:
+
+--Time complexity:
+
+--Space complexity:
+
+*/
+
+#include <iostream>
+
+// .. solution
+
+int main() {
+  // .. tests
+
+  return 0;
+}
